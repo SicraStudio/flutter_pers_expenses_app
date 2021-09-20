d6ccd20eb1a3171be8cfbd39b9b63df6504ed125
David Arcis Moreno <david.arcis@clouddistrict.com>
2021-09-20 22:54:18 +0200

    [REFACTOR] refactor NewTransaction widget to be stateful

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function handleAddTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.handleAddTx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submit() {
     final enteredTitle = titleController.text;
@@ -15,10 +22,12 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    handleAddTx(
+    widget.handleAddTx(
       enteredTitle,
       enteredAmount,
     );
+
+    Navigator.of(context).pop();
   }
 
   @override
